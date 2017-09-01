7c4073d9e09f6f06b75d27d4374d071a074065f5
cuiwenzhu <1187912456@qq.com>
2017-09-01 11:29:55 +0800

    rikao3效果

--- js1.js
+++
@@ -1,9 +0,0 @@
-/*
-* @Author: Marte
-* @Date:   2017-08-30 16:10:38
-* @Last Modified by:   Marte
-* @Last Modified time: 2017-08-30 16:11:04
-*/
-
-'use strict';
-console.log("js1")
--- js2.js
+++
@@ -1,9 +0,0 @@
-/*
-* @Author: Marte
-* @Date:   2017-08-30 16:10:38
-* @Last Modified by:   Marte
-* @Last Modified time: 2017-08-30 16:13:29
-*/
-
-'use strict';
-console.log("js2")
